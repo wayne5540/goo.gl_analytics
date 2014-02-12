1fa7510d9cd98eec303ea8947c07dba02ffc86c1
airson-nitrous <w5540@yahoo.com.tw>
2014-02-12 14:54:28 +0000

    add x.co and trying to fix goo.gl bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Helloworld::Application.routes.draw do
   root 'home#index'
+  get  "home/geturl"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
